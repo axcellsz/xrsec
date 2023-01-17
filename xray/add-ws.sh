d0e328c8d0931d31bf2050175fa51b2cc33212f9
axcellsz <122135545+axcellsz@users.noreply.github.com>
2023-01-17 22:48:00 +0700

    Update add-ws.sh

--- xray/add-ws.sh
+++ xray/add-ws.sh
@@ -50,6 +50,7 @@ clear
 
 
 uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "Expired (days): " masaaktif
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#vmess$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/config.json
